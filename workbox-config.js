dac2423429507d6b36cb5db9d33c06fd4d776799
XyaHPaK <xyahpak@gmail.com>
2018-12-05 11:54:06 +0200

    some Updates

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "dist/",
   "globPatterns": [
-    "**/*.{jpg,png,html,js,css}"
+    "**/*.{png,jpg,html,js,css}"
   ],
   "swDest": "dist\\sw.js",
   "navigateFallback": "index.html"
